a17f5f851669257ffce314c2ee588ac56bc103fc
Dark Ghost <106507701+Siluna2005@users.noreply.github.com>
2022-12-26 15:00:59 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,13 +20,13 @@ echo -e $lb "╚══════╝      ╚═════╝ ╚═╝  ╚�
 echo -e $r  "                                          Tool By - Dark Ghost"
 echo
 echo -e $y "What is Your Banner name ? : \c"
-read Bname
+read varBname
 echo
 echo -e $y "What is Your name ? : \c"
-read name
+read varname
 
-echo "cowsay -f eyes "$name" | lolcat " > name.txt
-echo "figlet "$Bname" |lolcat" > Bname.txt
+echo "cowsay -f eyes "$varname" | lolcat " > name.txt
+echo "figlet "$varBname" |lolcat" > Bname.txt
 echo "clear" > clear.txt
 echo "PS1='\$ '" > Temp.txt
 
